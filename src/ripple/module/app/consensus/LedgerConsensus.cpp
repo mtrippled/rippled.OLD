734fc4cf99824781de10afc9ac9e01110522f838
Mark Travis <mtravis@ripple.com>
2014-06-11 00:07:17 -0700

    Revert "Add temporary debug entries to learn why some ledgers take too long to close."
    This reverts commit eddaed114368efe1803526e199a3a4922f65070a.
    
    I put it in the wrong branch.

--- src/ripple/module/app/consensus/LedgerConsensus.cpp
+++ src/ripple/module/app/consensus/LedgerConsensus.cpp
@@ -1524,9 +1524,6 @@ private:
 
     void updateOurPositions ()
     {
-        WriteLog (lsDEBUG, LedgerConsensus) << "updateOurPositions seq"
-            << mPreviousLedger->getLedgerSeq () + 1
-            << " mPeerPositions.size(): " << mPeerPositions.size ();
         boost::posix_time::ptime peerCutoff
             = boost::posix_time::second_clock::universal_time ();
         boost::posix_time::ptime ourCutoff
@@ -1557,12 +1554,6 @@ private:
             {
                 // proposal is still fresh
                 ++closeTimes[roundCloseTime (it->second->getCloseTime ())];
-                WriteLog (lsDEBUG, LedgerConsensus)
-                    << "updateOurPositions fresh proposal seq"
-                    << mPreviousLedger->getLedgerSeq () + 1
-                    << " mPeerPositions.size(): " << mPeerPositions.size ()
-                    << "added closeTimes " << roundCloseTime(it->second->getCloseTime ())
-                    << "," << closeTimes[roundCloseTime (it->second->getCloseTime ())];
                 ++it;
             }
         }
@@ -1626,12 +1617,6 @@ private:
             if (mProposing)
             {
                 ++closeTimes[roundCloseTime (mOurPosition->getCloseTime ())];
-                WriteLog (lsDEBUG, LedgerConsensus)
-                    << "updateOurPositions proposing seq"
-                    << mPreviousLedger->getLedgerSeq () + 1
-                    << " mPeerPositions.size(): " << mPeerPositions.size ()
-                    << "added closeTimes " << roundCloseTime(it->second->getCloseTime ())
-                    << "," << closeTimes[roundCloseTime (it->second->getCloseTime ())];
                 ++threshVote;
                 ++threshConsensus;
             }
